8ad5e9dc9b6cab55b4808da03fdb31e24445f1ea
Chai Phonbopit <phonbopit@gmail.com>
2018-02-11 00:28:21 +0700

    add bulma

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="my-app">
+        <nav className="navbar is-light" role="navigation" aria-label="main navigation">
+          <div className="container">
+            <div className="navbar-brand">
+              <a class="navbar-item" href="https://devahoy.com">
+                <img src="https://devahoy.com/assets/images/devahoy-text-logo.png" alt="DEVAHOY LOGO" width="112" height="28" />
+              </a>
+            </div>
+            <div className="navbar-menu">
+              <div className="navbar-end">
+                <a className="navbar-item" href="#">Home</a>
+                <a className="navbar-item" href="#">Posts</a>
+                <a className="navbar-item" href="#">Projects</a>
+                <a className="navbar-item" href="#">About</a>
+                <a class="navbar-item" href="https://github.com/phonbopit" target="_blank">Star on <i className="fab fa-github"></i></a>
+              </div>
+            </div>
+          </div>
+        </nav>
+
+        <div className="container">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
       </div>
     )
   }
