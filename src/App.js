4aa5290bacb786914738c749fa2a3ed83a52f00a
Chai Phonbopit <phonbopit@gmail.com>
2018-02-14 21:57:35 +0700

    add active ClassName

--- src/App.js
+++ src/App.js
@@ -10,16 +10,16 @@ class App extends Component {
           <div className="container">
             <div className="navbar-brand">
               <a className="navbar-item" href="https://devahoy.com">
-                <img src="https://devahoy.com/assets/images/devahoy-text-logo.png" alt="DEVAHOY LOGO" width="112" height="28" />
+                <img src={process.env.PUBLIC_URL + '/images/devahoy-text-logo.png'} alt="DEVAHOY LOGO" width="112" height="28" />
               </a>
             </div>
             <div className="navbar-menu">
               <div className="navbar-end">
-                <NavLink to="/" className="navbar-item">Home</NavLink>
-                <NavLink to="/posts" className="navbar-item">Posts</NavLink>
-                <NavLink to="/projects" className="navbar-item">Projects</NavLink>
-                <NavLink to="/about" className="navbar-item">About</NavLink>
-                <a class="navbar-item" href="https://github.com/phonbopit" target="_blank">Star on <i className="fab fa-github"></i></a>
+                <NavLink to="/" activeClassName="is-active" className="navbar-item">Home</NavLink>
+                <NavLink to="/posts" activeClassName="is-active" className="navbar-item">Posts</NavLink>
+                <NavLink to="/projects" activeClassName="is-active" className="navbar-item">Projects</NavLink>
+                <NavLink to="/about" activeClassName="is-active" className="navbar-item">About</NavLink>
+                <a className="navbar-item" href="https://github.com/phonbopit" target="_blank">Star on <i className="fab fa-github"></i></a>
               </div>
             </div>
           </div>
